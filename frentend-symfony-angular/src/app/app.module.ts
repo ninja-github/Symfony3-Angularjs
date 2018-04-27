9665b1fe1e59bdf82dbea666d15843fb34ca1f7a
wiselh <wise.self.2@gmail.com>
2018-04-27 20:17:24 +0100

    Generate Components, Services & Auth Guard

--- frentend-symfony-angular/src/app/app.module.ts
+++ frentend-symfony-angular/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+//added
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { DeletePostComponent } from './delete-post/delete-post.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/auth/auth.service';
+import { AuthGuard } from './guard/auth.guard';
+import { PostService } from './services/post/post.service';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    AddPostComponent,
+    EditPostComponent,
+    DeletePostComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AuthService, PostService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
